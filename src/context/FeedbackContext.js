b627216af76bb91bae6ebfbd1ffbe9730b8a3e72
victoriad05 <dunaeva.victoria@gmail.com>
2022-05-05 12:22:35 -0500

    first commit

---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,72 @@
+import { createContext, useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const FeedbackContext = createContext();
+
+export const FeedbackProvider = ({ children }) => {
+  const [feedback, setFeedback] = useState([
+    {
+      id: 1,
+      text: "This is text one",
+      rating: 10,
+    },
+
+    {
+      id: 2,
+      text: "This is text two",
+      rating: 5,
+    },
+
+    {
+      id: 3,
+      text: "This is text three ",
+      rating: 7,
+    },
+  ]);
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+        feedback.map((item) => (item.id === id?{...item, ...updItem} : item))
+    )
+  }
+
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
+
+  return (
+    <FeedbackContext.Provider
+      value={{
+        feedback,
+        feedbackEdit,
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+        updateFeedback,
+        
+      }}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  );
+};
+
+export default FeedbackContext;
