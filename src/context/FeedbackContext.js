8b3b341a330dfa3b14498b7e7e30ff500eee131a
victoriad05 <dunaeva.victoria@gmail.com>
2022-05-09 18:57:19 -0500

    updateFeedback

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -43,19 +43,17 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   const updateFeedback = async (id, updItem) => {
-   const response = await fetch (`/feedback/&{id}`, {
-     method: 'PUT',
-     headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(updItem),
-  })
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem),
+    })
 
-  const data = await response.json()
+    const data = await response.json()
 
-    setFeedback(
-        feedback.map((item) => (item.id === id?{...item, ...data} : item))
-    )
+    setFeedback(feedback.map((item) => (item.id === id ? data : item)))
   }
 
   const editFeedback = (item) => {
