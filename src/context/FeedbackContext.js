95422581593a9e59ffe140476b726c36b888d0ff
victoriad05 <dunaeva.victoria@gmail.com>
2022-05-09 18:30:49 -0500

    isLoading

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -16,7 +16,7 @@ export const FeedbackProvider = ({ children }) => {
   },[])
 
   const fetchFeedback = async () => {
-    const response = await fetch (`http://localhost:5000/feedback?_sort=id&_order=desc`)
+    const response = await fetch (`/feedback?_sort=id&_order=desc`)
     const data = await response.json()
     setFeedback(data)
     setIsLoading(false)
